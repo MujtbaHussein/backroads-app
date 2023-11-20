e6a234f44f41102db2b8a4b0021ef9a492d5e0cf
Mujtaba <Mooji@gmail.com>
2023-11-20 15:44:56 +0330

    first-commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import About from "./Components/About";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import Services from "./Components/Services";
+import Tours from "./Components/Tours";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,50 @@
+import { pageLinks } from "../data";
+import { socialLinks } from "../data";
+import logo from "../images/logo.svg";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* left this comment on purpose  */}
+        <ul className="nav-links" id="nav-links">
+          {pageLinks.map(({ href, text, id }) => {
+            return (
+              <li key={id}>
+                <a href={href} className="nav-link">
+                  {" "}
+                  {text}{" "}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <ul className="nav-icons">
+          {socialLinks.map(({ id, href, icon }) => {
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  className="nav-icon"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
